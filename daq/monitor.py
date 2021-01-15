096f6d9a7bc920e8dce4852ff1d0b0c9d216b581
f-ikeda <71382123+f-ikeda@users.noreply.github.com>
2021-01-15 16:36:10 +0900

    Add files via upload
    昨日の僕は非常に周りくどいことをしていて、ビットストリーム→文字列→1 byte区切り→13 byte区切りに直すということをしていた。
    これを、ビットストリーム→文字列→13 byte区切りに修正。データ整形にかかる時間が(手元のmacbookで)1/20になった。処理速度は(手元のmacbookで)20 MB/sほど。
    10 shootのデータ(400 MBほど)なら(手元のmacbookで)20 秒で整形できるようになった。(これ以上早くしたいなら、np.frombuffer()を使うか、あるいは内包表記を別の何かにする必要があるが、それは余裕ができたら)
    次は、整形したデータに細工を施してプロットするとこに手を付ける。(ビット演算とnp.where()とファンシーインデックスを使う、ここでどれだけ時間がかかるか)

--- daq/monitor.py
+++ daq/monitor.py
@@ -34,22 +34,6 @@ BITS_MASK_SIG_MRSYNC = 2 ** BITS_SIZE_SIG_MRSYNC - 1
 # only the lower BIT_SIZE_SIG_MRSYNC bit is filled with 1
 
 
-def hex_then_str(number):
-    # ばかげた関数
-    return format(str(format(number, 'x')), '0>2')
-
-
-def shaper_13elements_to_1elements(elements):
-    # 13 要素を受け取って、1 要素にして返す
-    # 文字列の結合を利用して、1 byteに区切られたものを13 byteにまとめている
-    # 非常にばかげている
-    # 4 byteで動作説明: 入力[0xDE, 0xAD, 0xBE, 0xEF] 出力0xDEADBEEF
-    elements_str = map(hex_then_str, elements)
-    elements_str_list = list(elements_str)
-    element = ''.join(elements_str_list)
-    return int('0x'+element, 16)
-
-
 def hit_newhod_allor(sig):
     # return 1 if there is a hit on mppc's allor
     if (BITS_MASK_SIG_NEWHOD_ALLOR & sig != 0):
@@ -74,31 +58,19 @@ if(len(argument) != 3):
 file_path = argument[2]
 file = open(file_path, 'rb')
 
-# --------データの読み込み--------
+# --------READING DATA--------
 data_num = int(argument[1])
 # number of tdc data to read
 TIME_READ_S = time.time()
-data_bytes = file.read(DATA_UNIT * data_num)
+data_str = file.read(DATA_UNIT * data_num).hex()
+# str.hex(), only in python3
 TIME_READ_F = time.time()
 print("READ TIME [s]: " + str(TIME_READ_F - TIME_READ_S))
 
-# --------bitstreamを、1 要素が1 byteの配列にunpack--------
-TIME_UNPACK_S = time.time()
-format_string = str(DATA_UNIT * data_num) + 'B'
-data_array_1bytes = struct.unpack(format_string, data_bytes)
-data_array_1bytes = np.array(data_array_1bytes)
-TIME_UNPACK_F = time.time()
-print("UNPACK TIME [s]: " + str(TIME_UNPACK_F - TIME_UNPACK_S))
-
-# --------1 要素が1 byteの配列を、1 要素が1 byteの要素数13 の配列にする、つまり二次元配列--------
-# --------その後、各要素を13 byteに縮約する--------
+# --------FORMATTING DATA--------
 TIME_FORMAT_S = time.time()
-data_array_13bytes = data_array_1bytes.reshape(
-    [(int)(data_array_1bytes.size / DATA_UNIT), DATA_UNIT])
-# 1 要素が、1 要素が1 bytesの要素数 13の配列の、二次元配列
-data = np.array(list(map(shaper_13elements_to_1elements, data_array_13bytes)))
-# 1 要素が13 byteの配列
-# ここをfrompyfuncにするともっと早くなる
+data = np.array([int('0x' + data_str[i:i+26], 16)
+                 for i in range(0, len(data_str), 26)])
 # あるいは、dtypeを自作してnp.frombuffer()を使う
 TIME_FORMAT_F = time.time()
 print("FORMAT TIME [s]: " + str(TIME_FORMAT_F - TIME_FORMAT_S))
@@ -116,4 +88,5 @@ TIME_DRAW_F = time.time()
 print("DRAW TIME [s]: " + str(TIME_DRAW_F - TIME_DRAW_S))
 plt.show()
 '''
+
 file.close()
