9fc2d9deb66934734aa2c8078791ff0a98be6b4c
f-ikeda <71382123+f-ikeda@users.noreply.github.com>
2021-01-19 01:10:07 +0900

    Add files via upload
    データの保存場所を、500MB(変更可)ごとに変更するようにした。
    ctrl+cを押された時点で受け取っているデータを、正しく保存されるように修正した。
    
    TODO: nc+リダイレクトと同等くらいの速度でデータ取得できるか試す

--- daq/daqer.py
+++ daq/daqer.py
@@ -1,28 +1,38 @@
 '''
+ABOUT: using socket with blocking-mode、just repeat recv and saving data.
+       the file name will be YearMonthDay_HourMonthSecondMillisecond.dat at the first time when data saved.
+       when the size of one file reaches 500 [MB], the next new file is created.
+       this program does NOT make any decisions about Headers or Footers. it just receives and saves the data as it comes in.
+       the program will continue to run until ctrl+c is pressed.
+
 todo
-・そもそも動作するのか
-・動作するなら、取りこぼしがないか
-・動作するなら、データの保存先を定期的に変える
+・取りこぼしがないか
+その他
+・タイムアウトの挙動について(データなしで起動してみる)
 '''
-
+import os
 import socket
-
 import datetime
 
 DATA_UNIT = 13
 # bytes
+SAFETY_FACTOR = 400
+# the ratio of the 'real' buffer size to the amount of data received at any one time
 
 
 class Client(object):
 
-    def __init__(self, ip_address="192.168.10.16", tcp_port=24, buffer_data_num=1000):
+    def __init__(self, ip_address='192.168.10.16', tcp_port=24, buffer_data_num=100, file_single_size=500*1024*1024):
         self._ip_address = ip_address
         self._tcp_port = tcp_port
 
         self._buffer_size = DATA_UNIT * buffer_data_num
+        # number of data received at one time
 
         self._file_path = None
         self._file = None
+        self._file_single_size = file_single_size
+        # maximum size of a single file, in [bytes]
 
         self._sock = None
         # socket
@@ -38,20 +48,20 @@ class Client(object):
         self._sock.close()
 
     def save(self, data):
-        '''
-        if (self._firstheader_flag != 0) and (self._eventnum % self._spillchunk == 0):
-            self._file_path = "test_data/" + \
-                datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-            client._file = open(client._file_path, mode="wb")
-            client._file.close()
-        '''
+        if (os.path.getsize(self._file_path) > self._file_single_size):
+            self._file.close()
+            print('DATA SAVED: ' + self._file_path)
+            self._file_path = 'daqqed_data/' + \
+                datetime.datetime.now().strftime('%Y%m%d_%H%M%S%f') + '.dat'
+            # year, month, day, _, hour, second, millisecond
+
+            self._file = open(client._file_path, mode='wb')
+            # re-open with new name
 
         self._file.write(data)
 
     def recieve(self):
-
-        bytes_flagment = bytes()
-        # recieved data will be stored in this bytes
+        print('DAQ START: ' + datetime.datetime.now().strftime('%Y%m%d_%H%M%S%f'))
 
         try:
             while True:
@@ -63,22 +73,30 @@ class Client(object):
 
         except KeyboardInterrupt:
             # when ctrl+c comes
-            print("Stopped daq!")
+            if (len(bytes_flagment) != 0):
+                self.save(bytes_flagment)
+                # because there may be data in bytes_flagment that has not yet been saved
+            print('DAQ STOP: ' + datetime.datetime.now().strftime('%Y%m%d_%H%M%S%f'))
 
 
 if __name__ == '__main__':
 
     client = Client()
 
+    print('CURRENT BUFFER SIZE [bytes]: ' + str(
+        int(client._sock.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF))))
+    print('BUFFER SIZE SET AS [bytes]: ' +
+          str(int(client._buffer_size * SAFETY_FACTOR)))
+    client._sock.setsockopt(
+        socket.SOL_SOCKET, socket.SO_RCVBUF, int(client._buffer_size * SAFETY_FACTOR))
+    # this is the real buffer size (in kernel)...
+
+    client._file_path = 'daqqed_data/' + \
+        datetime.datetime.now().strftime('%Y%m%d_%H%M%S%f') + '.dat'
+    # year, month, day, _, hour, minute, second, millisecond
+    client._file = open(client._file_path, mode='wb')
+
     client.connect()
-    print('buffer size default' +
-          str(client._sock.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)))
-    client._sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1040000)
-
-    client._file_path = "daqqed_data/" + \
-        datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    client._file = open(client._file_path, mode="wb")
-
     client.recieve()
     client.disconnect()
 
