653f4ad5400965f595d14aa42469ac5431833674
f-ikeda <71382123+f-ikeda@users.noreply.github.com>
2021-01-24 03:33:21 +0900

    Add files via upload
    コインシデンスの処理を実装(代入方法がおかしいのは明白なので、直せto自分)
    (numpyでインターセクト(集合演算のかつ)が用意されていたのでヨカッタ！)

--- daq/monitor.py
+++ daq/monitor.py
@@ -1,6 +1,7 @@
 import sys
 
 import numpy as np
+from functools import partial, reduce
 
 import matplotlib.pyplot as plt
 
@@ -8,7 +9,12 @@ import time
 # for debug
 
 # ########DESCRIPTION########
-# USEAGE: $ python3 moniter.py how_many_13bytes_chunk_to_read _path_to_data b(option: for loading December's data)
+# USEAGE: $ python3 moniter.py how_many_13bytes_chunk_to_read path_to_data
+#         $ python3 monitor.py 3580598 beamOn-10shoot-20201217.dat (1 spill)
+#         $ python3 monitor.py 7177025 beamOn-10shoot-20201217.dat (2 spill)
+#                                       :
+#         header: 0       3580598 7177025  10808540 14407270 18044246 21633446 25225710 28865318 32431605
+#         footer: 3580597 7177024 10808539 14407269 18044245 21633445 25225709 28865318 32431604 欠損？
 #
 # INPUT: raw binary file, such as there are
 #        00000000000001000000000123 <- hit on detector
@@ -27,17 +33,24 @@ import time
 #        00000FEE00AAAAAAAAAAAAAAAA <- Footer
 #        01000000000000000000000111 <- hit on detector
 #
-# OUTPUT ARRAY: sig        = [sig of all the corresponding tdc data]
-#               tdc        = [123, 12a, 450, 541, 711, 957, 601, 123, 340, 601, 111]
-#               mrsync     = [-1, 12a, 12a, 12a, 12a, 957, 957, 957, 340, 340, 340]
-#               spillcount = [-1, 3272,3272,3272,3272,3272,3272,3273,3273,3273]
+# OUTPUT ARRAY: sig        = [                            sig of all the corresponding tdc data                            ]
+#               tdc        = [123, header's, 12a, 450, 541, 711, 957, 601, footer's, header's, 123, 340, 601, footer's, 111]
+#               mrsync     = [-1,        -1, 12a, 12a, 12a, 12a, 957, 957,      957,      957, 957, 340, 340,      340, 340]
+#               spillcount = [-1,      3272,3272,3272,3272,3272,3272,3272,     3272,     3273,3273,3273,3273,     3273,3273]
+#
+# OVERVIEW: READING       ...reading a byte sequence
+#           FORMATTING    ...converting a sequence of bytes into an array with elements of 13-byte
+#           PROCCESING    ...creating an array of sig, tdc, mrsync and spillcount
+#           ANALYZING     ...
+#           DRAWING       ...drawing some plots
 #
 # ISSUES: コインシデンスの処理が未実装(indexがクロックカウントに等しい配列を、コインシデンスを撮りたい分だけ用意して、各要素のminをとる)
 #         データの読み込み、定期的に(how?)ディレクトリの中にある最新のものを探して(globでおk)、勝手に読むようにする
+#         12月のデータで動作確認をしているため、HeaderとFooterの仕様が古い
 #
 # 仕様: ファイル中に、HeaderとFooterとが、同数かつHeader,Footer,...,Header,Footerの順に、必ず1 組以上含まれていなければいけない
-#       ファイル中に、MR Syncが必ず1 つ含まれていなければいけない
-#       Headerよりも後、かつ、MR Syncよりも
+#       ファイル中に、MR Syncが必ず1 つ以上含まれていなければいけない
+#       Headerよりも後、かつ、MR Syncよりも前にイベントがあった場合、mrsyncに前回のスピルの最後のMR Syncの値を割り当てる
 
 DATA_UNIT = 13
 # bytes
@@ -88,14 +101,122 @@ BITS_MASK_SIG_MRSYNC = 2 ** BITS_SIZE_SIG_MRSYNC - 1
 CLOCK_TIME = 5
 # ns
 
-PLOT_LENGTH_FROM_MRSYNC = 6
-# us
-
 DATA_TYPE = np.dtype((np.void, DATA_UNIT))
 
 
 def bytes_to_int(DEADBEEF):
     return int.from_bytes(DEADBEEF, 'big')
+
+
+def formatting_data(data_bytes):
+    data = np.frombuffer(data_bytes, DATA_TYPE)
+    data = bytes_to_int_universal(data)
+
+    return data
+
+
+def processing_spillcount():
+    # ----SPILLCOUNT----
+    condition_header = ((data & BITS_MASK_HEADER) == BITS_WORD_HEADER)
+    # making the boolian mask
+    condition_footer = ((data & BITS_MASK_FOOTER) == BITS_WORD_FOOTER)
+    # making the boolian mask
+    index_header = np.where(condition_header)
+    # getting the position of the Header
+    index_footer = np.where(condition_footer)
+    # getting the position of the Footer
+    list_spillcount = (np.extract(condition_header, data)
+                       & BITS_MASK_SPILLCOUNT)
+    # getting the list of the Spillcount
+    spillcount = np.concatenate([np.full(index_header[0][0], -1), np.repeat(
+        list_spillcount, np.diff(index_header[0], append=data.size))])
+    # when there are no Header in file, index_header[0][0] causes an error
+
+    return spillcount, index_header, index_footer, condition_header, condition_footer, list_spillcount
+
+
+def processing_sig():
+    # ----SIG----
+    sig = (data & BITS_MASK_SIG) >> BITS_SIZE_TDC
+
+    return sig
+
+
+def processing_tdc():
+    # ----TDC----
+    tdc = data & BITS_MASK_TDC
+
+    # ----TDC CLOCK COUNT OVERFLOW----
+    iter_index_header = iter(index_header[0])
+    iter_index_fotter = iter(index_footer[0])
+    index_header_and_footer = np.array(
+        [i for i in zip(iter_index_header, iter_index_fotter)])
+
+    index_within_a_spill = [tdc[array_i[0]+1:array_i[1]]
+                            for array_i in index_header_and_footer]
+    # +1 for the first TDC data from Header
+    index_tdcdiff_within_a_spill = [np.where(np.diff(array_i) < 0)[
+        0] for array_i in index_within_a_spill]
+    # np.diff() returns local index in input
+
+    index_overflow_and_footer = [np.insert(array_i+index_header[0][i]+1+1, array_i.size, index_footer[0][index_k])
+                                 for array_i, i, index_k in zip(index_tdcdiff_within_a_spill, range(index_header[0].size), range(index_footer[0].size))]
+    # rewrite local index as global index in all data
+    for array_i in index_overflow_and_footer:
+        for index_k in range(len(array_i)-1):
+            tdc[array_i[index_k]:array_i[index_k+1]] = tdc[array_i[index_k]                                                           :array_i[index_k+1]] + (index_k+1) * 2 ** 27
+    # mapか何かで書き直せるはず
+
+    return tdc
+
+
+def processing_mrsync():
+    # ----MR SYNC----
+    condition_mrsync = ((sig & BITS_MASK_SIG_MRSYNC) == BITS_MASK_SIG_MRSYNC) & (
+        ~condition_header) & (~condition_footer)
+    # making the boolian mask
+    index_mrsync = np.where(condition_mrsync)
+    # getting the position of the MR Sync
+    list_mrsync = np.extract(condition_mrsync, tdc)
+    # getting the list of the MR Sync's TDC
+    mrsync = np.concatenate([np.full(index_mrsync[0][0], -1), np.repeat(
+        list_mrsync, np.diff(index_mrsync[0], append=data.size))])
+    # when there are no MR Sync data in file, index_mrsync[0][0] causes an error
+
+    return mrsync, condition_mrsync, list_mrsync
+
+
+def removing_header_and_footer():
+    # this function may not be necessary, because there are boolian masks, such as conditon_header and condition_footer
+
+    # ----REMOVING HEADER AND FOOTER----
+    spillcount = np.delete(spillcount, np.concatenate(
+        [index_header[0], index_footer[0]]))
+    # removing Header and Footer
+    sig = np.delete(sig, np.concatenate([index_header[0], index_footer[0]]))
+    # removing Header and Footer
+    tdc = np.delete(tdc, np.concatenate([index_header[0], index_footer[0]]))
+    # removing Header and Footer
+    mrsync = np.delete(mrsync, np.concatenate(
+        [index_header[0], index_footer[0]]))
+    # removing Header and Footer
+
+
+def coincidence(conditions):
+    # ----COINCIDENCE----
+    intersect1d_merge = partial(np.intersect1d, assume_unique=True)
+    # in a given spill, the set of values of TDC is considered unique because it is monotonically increasing in a narrow sense
+
+    for spill_k in list_spillcount:
+        # coincidence has to be considered for each spill independently
+        condition_spill_k = ((spillcount == spill_k) & ~
+                             condition_header & ~condition_footer)
+        tdc_coincidenced = reduce(intersect1d_merge, tuple(
+            [np.extract(condition_i & condition_spill_k, tdc) for condition_i in conditions]))
+        # ここで等号を用いるのはおかしい、スライスして代入するようにしろ
+        # spillcount配列から、あるスピルについて[先頭:末尾]するべき先頭と末尾は決まるはず
+
+    return tdc_coincidenced
 
 
 argument = sys.argv
@@ -109,9 +230,9 @@ file = open(file_path, 'rb')
 
 file.read(DATA_UNIT)
 # beamOn-10shoot-20201217.datを用いるために、ファイル冒頭のフッターを読み飛ばす
-# header [0]
-# footer [3580597]
-# 読み込むデータは3580598個
+# $ python3 monitor.py 3580598 beamOn-10shoot-20201217.dat (1 spill)
+# $ python3 monitor.py 7177025 beamOn-10shoot-20201217.dat (2 spill)
+#                               :
 # header: 0       3580598 7177025  10808540 14407270 18044246 21633446 25225710 28865318 32431605
 # footer: 3580597 7177024 10808539 14407269 18044245 21633445 25225709 28865318 32431604 欠損？
 
@@ -122,120 +243,54 @@ data_num = int(argument[1])
 # number of tdc data to read
 TIME_READ_S = time.time()
 data_bytes = file.read(DATA_UNIT * data_num)
-# str.hex(), only in python3
 TIME_READ_F = time.time()
 print("READ TIME [s]: " + str(TIME_READ_F - TIME_READ_S))
 
+# --------FORMATTING--------
 bytes_to_int_universal = np.frompyfunc(bytes_to_int, 1, 1)
 # converting function to universal function
 
-# --------FORMATTING--------
 TIME_FORMAT_S = time.time()
-data = np.frombuffer(data_bytes, DATA_TYPE)
-data = bytes_to_int_universal(data)
+data = formatting_data(data_bytes)
 TIME_FORMAT_F = time.time()
 print("FORMAT TIME [s]: " + str(TIME_FORMAT_F - TIME_FORMAT_S))
 
 # --------PROCESSING--------
 TIME_PROCESS_S = time.time()
 # ----SPILLCOUNT----
-condition_header = ((data & BITS_MASK_HEADER) == BITS_WORD_HEADER)
-# making the boolian mask
-condition_footer = ((data & BITS_MASK_FOOTER) == BITS_WORD_FOOTER)
-# making the boolian mask
-header_index = np.where(condition_header)
-# getting the position of the Header
-footer_index = np.where(condition_footer)
-# getting the position of the Footer
-spillcount_list = (np.extract(condition_header, data)
-                   & BITS_MASK_SPILLCOUNT)
-# getting the list of the Spillcount
-spillcount = np.concatenate([np.full(header_index[0][0], -1), np.repeat(
-    spillcount_list, np.diff(header_index[0], append=data.size))])
-# when there are no Header data in file, header_index[0][0] causes an error
-
+spillcount, index_header, index_footer, condition_header, condition_footer, list_spillcount = processing_spillcount()
 # ----SIG----
-sig = (data & BITS_MASK_SIG) >> BITS_SIZE_TDC
-
+sig = processing_sig()
 # ----TDC----
-tdc = data & BITS_MASK_TDC
-
-# ----TDC CLOCK COUNT OVERFLOW----
-
-# h&fがセットでいることを前提とする
-header_index_iter = iter(header_index[0])
-footer_index_iter = iter(footer_index[0])
-
-array_foo = np.array([i for i in zip(header_index_iter, footer_index_iter)])
-print('array_foo: ' + str(array_foo))
-print('array_foo[0]: ' + str(array_foo[0]))
-
-array_bar = [tdc[i_array[0]+1:i_array[1]] for i_array in array_foo]
-# headerの次から、のために+1。後ろはフッター含まずなのでそのまま
-print('array_bar: ' + str(array_bar))
-
-array_bar = [np.where(np.diff(i_array) < 0)[0] for i_array in array_bar]
-print('array_bar: ' + str(array_bar))
-
-array_bar = [np.insert(i_array+header_index[0][i]+1+1, i_array.size, footer_index[0][k])
-             for i_array, i, k in zip(array_bar, range(header_index[0].size), range(footer_index[0].size))]
-print('array_bar: ' + str(array_bar))
-
-# array_bar = [tdc[j:j+1] + 2 **
-#             27 for i_array in array_bar for j in range(len(i_array) - 1)]
-
-for i_array in array_bar:
-    for j in range(len(i_array)-1):
-        tdc[i_array[j]:i_array[j+1]] = tdc[i_array[j]:i_array[j+1]] + (j+1) * 2 ** 27
-
-# print('tdc: ' + str(tdc))
-
+tdc = processing_tdc()
 # ----MR SYNC----
-condition_mrsync = ((sig & BITS_MASK_SIG_MRSYNC) == BITS_MASK_SIG_MRSYNC) & (
-    ~condition_header) & (~condition_footer)
-# making the boolian mask
-mrsync_index = np.where(condition_mrsync)
-# getting the position of the MR Sync
-mrsync_list = np.extract(condition_mrsync, tdc)
-# getting the list of the MR Sync's TDC
-mrsync = np.concatenate([np.full(mrsync_index[0][0], -1),
-                         np.repeat(mrsync_list, np.diff(mrsync_index[0], append=data.size))])
-# when there are no MR Sync data in file, mrsync_index[0][0] causes an error
-# P3より後かつMR Syncより前のイベントについて、一つ前のスピルでの最後のMR Syncを割り当ててしまう(そのようなイベントはないはず(？))
+mrsync, condition_mrsync, list_mrsync = processing_mrsync()
+TIME_PROCESS_F = time.time()
+print("PROCESS TIME [s]: " + str(TIME_PROCESS_F - TIME_PROCESS_S))
 
-'''
-# ----REMOVING HEADER AND FOOTER----
-spillcount = np.delete(spillcount, np.concatenate(
-    [header_index[0], footer_index[0]]))
-# removing Header and Footer
-sig = np.delete(sig, np.concatenate([header_index[0], footer_index[0]]))
-# removing Header and Footer
-tdc = np.delete(tdc, np.concatenate([header_index[0], footer_index[0]]))
-# removing Header and Footer
-mrsync = np.delete(mrsync, np.concatenate([header_index[0], footer_index[0]]))
-# removing Header and Footer
-'''
-# these processes may not be necessary, because there are boolian masks, such as conditon_header and condition_footer
-
+# --------ANALYZING--------
+TIME_ANALYZE_S = time.time()
 # ----COINCIDENCE----
 # ここで、コインシデンスをとるような処理を書く
 # array_foo = fromnumpyfunc(foo array_foo,2,1)でdef array_foo[foo]みたいに
 
-
 # ########Write the analysis code here using sig, tdc, mrsync and spillcount########
 condition_newhod_allor = ((sig & BITS_MASK_SIG_NEWHOD_ALLOR)
                           != 0) & ~condition_header & ~condition_footer
-newhod_allor = np.extract(condition_newhod_allor, tdc - mrsync)
+# making the boolian mask
+newhod_allor = np.extract(condition_newhod_allor, tdc)
+newhod_allor = coincidence((condition_newhod_allor, condition_newhod_allor))
 # ##################################################################################
-TIME_PROCESS_F = time.time()
-print("PROCESS TIME [s]: " + str(TIME_PROCESS_F - TIME_PROCESS_S))
+TIME_ANALYZE_F = time.time()
+print("ANALYZE TIME [s]: " + str(TIME_ANALYZE_F - TIME_ANALYZE_S))
 
 # --------DRAWING--------
 TIME_DRAW_S = time.time()
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
-ax.hist(newhod_allor*5*pow(10, -3), bins=250, histtype='step', log=True)
+ax.hist(newhod_allor*CLOCK_TIME*pow(10, -9),
+        bins=250, histtype='step', log=True)
 ax.set_ylim(0.1, None)
 TIME_DRAW_F = time.time()
 print("DRAW TIME [s]: " + str(TIME_DRAW_F - TIME_DRAW_S))
@@ -244,7 +299,7 @@ plt.show()
 # ここに、chマップをみるための二次元ヒストグラムを書く
 # fig_bar =
 
-
+'''
 print('-------- DEBUG --------')
 print('data.dtype: ' + str(data.dtype))
 print('sig.dtype: ' + str(sig.dtype))
@@ -258,8 +313,8 @@ print('spillcount.size: ' + str(spillcount.size))
 print('mrsync: ' + str(mrsync))
 print('spillcount: ' + str(spillcount))
 
-print('mrsync_list: ' + str(mrsync_list))
-print('spillcount_list: ' + str(spillcount_list))
+print('list_mrsync: ' + str(list_mrsync))
+print('list_spillcount: ' + str(list_spillcount))
 
 print('np.unique(mrsync).size: ' +
       str(np.unique(mrsync).size))
@@ -271,9 +326,9 @@ print('np.unique(mrsync, return_index=True)[1]: ' + str(
 print('np.unique(spillcount, return_index=True)[1]: ' + str(
     np.unique(spillcount, return_index=True)[1]))
 
-print('header_index[0]: ' + str(header_index[0]))
-print('footer_index[0]: ' + str(footer_index[0]))
-print('mrsync_index[0]: ' + str(mrsync_index[0]))
-
+print('index_header[0]: ' + str(index_header[0]))
+print('index_footer[0]: ' + str(index_footer[0]))
+print('index_mrsync[0]: ' + str(index_mrsync[0]))
+'''
 
 file.close()
