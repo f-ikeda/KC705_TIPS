b3684849983e39c144f8048cd71ff08e32b44d10
Fumihito Ikeda <f-ikeda@Fumi-MacBookPro.local>
2021-04-21 23:47:01 +0900

    mod: now, diff can be cecked

--- spilldiv_check.py
+++ spilldiv_check.py
@@ -1,6 +1,10 @@
 # purpose: to check the behaviour of spill division function in MCS-mode
 
-for mrsync in range(500000):
+count_old = 0
+for i in range(500000):
+    mrsync = i
     if ((mrsync & 0b11111111111) == 0b00000000000) \
             & (((mrsync >> 11) & 0b1) == 0b1):
-        print('mrsync:', mrsync)
+        print('mrsync:', mrsync,
+              'diff:', (mrsync - count_old))
+        count_old = mrsync
